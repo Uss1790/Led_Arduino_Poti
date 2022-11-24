cd93727c9558bd9ca1c59abe4e19ef6b91680313
Uss1790 <114951501+Uss1790@users.noreply.github.com>
2022-11-24 10:32:22 +0100

    Create Led_mit_Poti.ino

---
+++ Led_mit_Poti.ino
@@ -0,0 +1,34 @@
+void setup() {
+  Serial.begin(9600);
+  for (int i = 2; i < 11; i++) {
+    pinMode(i, OUTPUT);
+  }
+  pinMode(11, INPUT);
+}
+
+int sensorwert = 0;
+
+
+void loop() {
+  zu_array(wie_viele(poti()));
+}
+
+int poti() {
+  sensorwert = analogRead(0);
+  return sensorwert;
+}
+
+int wie_viele(int poti_wert) {
+  int wie_viele = 0;
+  wie_viele = map(poti_wert, 0, 1023, 0, 10);
+  return wie_viele;
+}
+
+void zu_array(int wie_viele){
+  int led_array[10] = {0,0,0,0,0,0,0,0,0,0};
+
+  for (int i = 0; i <= wie_viele; i++){
+    led_array[i] = 1;
+    Serial.println(led_array[4]);
+  }
+}
